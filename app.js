b266d3b97e5fcdade05a68997c7a113f9596c213
vishnusingh0101 <singh.vishnu8369@gmail.com>
2025-04-17 12:44:43 +0530

    update

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const startServer = async () => {
         await mongoose.connect(process.env.MONGODB);
         console.log("Database Connected");
 
-        app.listen(80, '0.0.0.0',() => {
+        app.listen(3000,() => {
             console.log("Server running on port 3000");
         });
     } catch (err) {
